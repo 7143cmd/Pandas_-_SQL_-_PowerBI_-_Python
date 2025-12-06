8e33d93531f1213d8d2dae9fe679c01edc3b7caa
Dmytro <pytakdima@gmail.com>
2025-12-06 15:51:29 +0200

    separate csv 2 sql func

--- pandas/to_database.py
+++ pandas/to_database.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import sqlite3
 
+def csv_2_sql(df):
+    conn  = sqlite3.connect('hotel_bookings.db')
 
-df = pd.read_csv('datasets/hotel_bookings.csv')
+    df.to_sql('hotel_bookings', conn, if_exists='replace', index=False)
 
-conn  = sqlite3.connect('hotel_bookings.db')
+    conn.close()
 
-df.to_sql('hotel_bookings', conn, if_exists='replace', index=False)
 
-conn.close()
+if __name__ == "__main__":
+    csv_2_sql(df = pd.read_csv('datasets/hotel_bookings.csv'))

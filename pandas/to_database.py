e79563a1de434d8eae3e2f5cc8c9bd1dc2f84b24
Dmytro <pytakdima@gmail.com>
2025-12-09 19:20:36 +0200

    New dataset added

--- pandas/to_database.py
+++ pandas/to_database.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import sqlite3
 
-def csv_2_sql(df):
-    conn  = sqlite3.connect('hotel_bookings.db')
+def csv_2_sql(df, dbname):
+    conn  = sqlite3.connect(dbname)
 
-    df.to_sql('hotel_bookings', conn, if_exists='replace', index=False)
+    df.to_sql('AI_impact', conn, if_exists='replace', index=False)
 
     conn.close()
 
 
 if __name__ == "__main__":
-    csv_2_sql(df = pd.read_csv('datasets/hotel_bookings.csv'))
+    csv_2_sql(pd.read_csv('datasets/AI_Impact_on_Jobs_2030.csv'), 'AI_impact.db')
--- pandas/transformation.py
+++ pandas/transformation.py
@@ -1,29 +1,33 @@
 import pandas as pd
 from to_database import csv_2_sql
 
-df = pd.read_csv('datasets/hotel_bookings.csv')
+def DB1():
+    df = pd.read_csv('datasets/hotel_bookings.csv')
 
-df = df.drop_duplicates()
+    df = df.drop_duplicates()
 
 
-for col in df.columns:
-    if df[col].dtype in ['int64', 'float64']:
-        df[col] = df[col].fillna(df[col].median())
-    else:
-        df[col] = df[col].fillna(df[col].mode()[0] if not df[col].mode().empty else 'Unknown')
+    for col in df.columns:
+        if df[col].dtype in ['int64', 'float64']:
+            df[col] = df[col].fillna(df[col].median())
+        else:
+            df[col] = df[col].fillna(df[col].mode()[0] if not df[col].mode().empty else 'Unknown')
 
-columns_to_drop = ['agent', 'company', 'reservation_status_date']
-df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
+    columns_to_drop = ['agent', 'company', 'reservation_status_date']
+    df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
 
-for col in df.select_dtypes(include=['float64']).columns:
-    df[col] = df[col].round(0).astype('int64')
+    for col in df.select_dtypes(include=['float64']).columns:
+        df[col] = df[col].round(0).astype('int64')
 
-for col in df.select_dtypes(include=['int64']).columns:
-    df[col] = pd.to_numeric(df[col], downcast='integer')
+    for col in df.select_dtypes(include=['int64']).columns:
+        df[col] = pd.to_numeric(df[col], downcast='integer')
 
-df['total_guests'] = df['adults'] + df['babies'] + df['children']
-df['total_nights'] = df['stays_in_weekend_nights'] + df['stays_in_week_nights']
+    df['total_guests'] = df['adults'] + df['babies'] + df['children']
+    df['total_nights'] = df['stays_in_weekend_nights'] + df['stays_in_week_nights']
 
-df = df.reset_index(drop=True)
+    df = df.reset_index(drop=True)
 
-csv_2_sql(df)
+    csv_2_sql(df, 'hotel_bookings')
+
+def DB2():
+    pass
